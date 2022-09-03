41cc551cc0f0a947dac2093ba444cc90dad7f6fb
Christoph <christoph.thonhauser@squer.at>
2022-09-03 22:17:15 +0200

    display graded average grade

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,20 @@
-<script>
+<script lang="ts">
     import Navbar from "./lib/Navbar.svelte";
+    import type {Course} from "./calculator/calculateWeightedGrade"
+    import {calculateAverageWeightedGrade} from "./calculator/calculateWeightedGrade"
+
+    const math: Course = {ECTS: 5, grade: 3};
+    const english: Course = {ECTS: 5, grade: 1};
+    const spanish: Course = {ECTS: 1, grade: 4};
+    const courses: Course[] = [math, english, spanish];
+
+    const weightedGrade = calculateAverageWeightedGrade(courses);
 </script>
 
-<Navbar/>
+<div class="flex flex-col justify-center gap-y-12">
+    <Navbar/>
+    <div class="flex gap-x-4 justify-center">
+        <p>Gewichtete Note:</p>
+        <div>{weightedGrade}</div>
+    </div>
+</div>
