3915d11045e424727eb681134fe6b21e7489f344
Christoph <christoph.thonhauser@squer.at>
2022-09-11 22:42:52 +0200

    closes #5; support multiple courses

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,61 @@
 <script lang="ts">
   import Navbar from "./lib/Navbar.svelte";
-  import type { Course } from "./calculator/calculateWeightedGrade";
   import { calculateAverageWeightedGrade } from "./calculator/calculateWeightedGrade";
-  import { weightedGrade } from "./store.js";
+  import { weightedGrade, courses } from "./store.js";
   import { Button } from "@brainandbones/skeleton";
   import Logo from "./lib/Logo.svelte";
+  import AddButton from "./lib/AddButton.svelte";
 
   let ECTS = 0;
   let grade = 0;
 
   const setGrade = () => {
-    let course: Course = {
-      ECTS,
-      grade
-    };
-    const result = calculateAverageWeightedGrade([course]);
+    const result = calculateAverageWeightedGrade($courses);
     weightedGrade.set(result);
+  };
+  const addCourse = () => {
+    courses.update((courses) => [
+      ...courses,
+      {
+        ECTS,
+        grade,
+      },
+    ]);
+    console.log(courses);
   };
 
 </script>
 
 <section class="flex flex-col justify-center gap-y-12 pt-48">
   <Navbar />
-  <div class="flex gap-4 justify-center">
+  {#each $courses as course}
+  <section class="flex gap-4 justify-center">
     <div>
       <label class="block text-sm font-bold mb-2" for="grade">
         ECTS
       </label>
-      <input id="ECTS" type="number" bind:value={ECTS}
+      <input id="ECTS" type="number" bind:value={course.ECTS}
              class="appearance-none text-primary-500 dark:text-accent-300 dark:bg-neutral-800 rounded w-72" />
     </div>
     <div>
       <label class="block text-sm font-bold mb-2" for="grade">
         Grade
       </label>
-      <input id="grade" type="number" min="1" max="5" bind:value={grade}
+      <input id="grade" type="number" min="1" max="5" bind:value={course.grade}
              class="appearance-none text-primary-500 dark:text-accent-300 dark:bg-neutral-800 rounded w-72" />
     </div>
-  </div>
+  </section>
+  {/each}
+  <Button
+    variant="filled-white"
+    on:click={addCourse}
+    width="w-32 self-center"
+  >
+    <svelte:fragment slot="lead">
+      <AddButton />
+    </svelte:fragment>
+    Add Course
+  </Button>
   <Button
     variant="filled-primary"
     on:click={setGrade}
--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,7 @@
 import { writable } from "svelte/store";
+import type { Course } from "./calculator/calculateWeightedGrade";
 
 export const weightedGrade = writable(0);
+
+const initCourse:Course = {ECTS: 0, grade: 0};
+export const courses = writable([initCourse]);
