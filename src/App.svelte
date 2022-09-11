1d8d4561e35d344a1c4e0a71e670a66f1d7fd425
Christoph <christoph.thonhauser@squer.at>
2022-09-11 22:51:25 +0200

    refactors

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,24 @@
 <script lang="ts">
   import Navbar from "./lib/Navbar.svelte";
   import { calculateAverageWeightedGrade } from "./calculator/calculateWeightedGrade";
-  import { weightedGrade, courses } from "./store.js";
+  import { courses, weightedGrade } from "./store.js";
   import { Button } from "@brainandbones/skeleton";
   import Logo from "./lib/Logo.svelte";
   import AddButton from "./lib/AddButton.svelte";
-
-  let ECTS = 0;
-  let grade = 0;
 
   const setGrade = () => {
     const result = calculateAverageWeightedGrade($courses);
     weightedGrade.set(result);
   };
   const addCourse = () => {
+    let ECTS = 0, grade = 0;
     courses.update((courses) => [
       ...courses,
       {
         ECTS,
-        grade,
-      },
+        grade
+      }
     ]);
-    console.log(courses);
   };
 
 </script>
@@ -29,25 +26,24 @@
 <section class="flex flex-col justify-center gap-y-12 pt-48">
   <Navbar />
   {#each $courses as course}
-  <section class="flex gap-4 justify-center">
-    <div>
-      <label class="block text-sm font-bold mb-2" for="grade">
-        ECTS
-      </label>
-      <input id="ECTS" type="number" bind:value={course.ECTS}
-             class="appearance-none text-primary-500 dark:text-accent-300 dark:bg-neutral-800 rounded w-72" />
-    </div>
-    <div>
-      <label class="block text-sm font-bold mb-2" for="grade">
-        Grade
-      </label>
-      <input id="grade" type="number" min="1" max="5" bind:value={course.grade}
-             class="appearance-none text-primary-500 dark:text-accent-300 dark:bg-neutral-800 rounded w-72" />
-    </div>
-  </section>
+    <section class="flex gap-4 justify-center">
+      <div>
+        <label class="block text-sm font-bold mb-2" for="grade">
+          ECTS
+        </label>
+        <input id="ECTS" type="number" bind:value={course.ECTS}
+               class="appearance-none text-primary-500 dark:text-accent-300 dark:bg-neutral-800 rounded w-72" />
+      </div>
+      <div>
+        <label class="block text-sm font-bold mb-2" for="grade">
+          Grade
+        </label>
+        <input id="grade" type="number" min="1" max="5" bind:value={course.grade}
+               class="appearance-none text-primary-500 dark:text-accent-300 dark:bg-neutral-800 rounded w-72" />
+      </div>
+    </section>
   {/each}
   <Button
-    variant="filled-white"
     on:click={addCourse}
     width="w-32 self-center"
   >
